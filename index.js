ade24e7de4e0517dbd7496a76c5199e3121a7ac9
Raunit Raj <raunitraj2336@gmail.com>
2026-02-12 10:11:24 +0530

    Testing the server

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import express from 'express';
 
 const app = express();
-const PORT = process.env.PORT ?? 8080
+const PORT = process.env.PORT ?? 3000;
 app.get('/',(req,res)=>{
     console.log("This message is from server");
     res.send('Hello from server');
